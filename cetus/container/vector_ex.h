306b1b3527a60294fe50c7a2bbbe1965d752c432
ottox <x20121223@163.com>
2018-01-16 23:24:40 +0800

    vector ex

--- cetus/container/vector_ex.h
+++ cetus/container/vector_ex.h
@@ -1,19 +1,70 @@
 #pragma once
 
 #include <vector>
+
 namespace terra
 {
 	template<class T, class Alloc = std::allocator<T>>
 	class TVector : public std::vector<T, Alloc>
 	{
 	public:
-		void RemoveAtSwap()
+		void swap_to_end(size_type index)
 		{
-			using std::swap;
-			swap(vec[i], vec.back());
-			vec.pop_back();
+			std::swap(at(index), back());
+		}
+		//FixMe size_type must be signed type
+		size_type push_unique(const T& val)
+		{	// insert element at end, provide strong guarantee
+			return push_unique_impl(_Val);
 		}
 
+		size_type push_unique(T&& val)
+		{	// insert by moving into element at end, provide strong guarantee
+			return push_unique_impl(std::move(val));
+		}
 
+		bool find(const T& val, size_type& index) const
+		{
+			index = this->find(val);
+			return index != -1;
+		}
+		size_type find(const T& val) const
+		{
+			for (const_iterator it = begin(); it != end(); ++it)
+			{
+				if (*it == val)
+				{
+					return (it - begin());
+				}
+			}
+			return -1;
+		}
+		bool find_last(const T& val, size_type& index) const
+		{
+			index = this->find(val);
+			return index != -1;
+		}
+		size_type find_last(const T& val) const
+		{
+			for (const_reverse_iterator it = rbegin(); it != rend(); ++it)
+			{
+				if (*it == val)
+				{
+					return (it - rend());
+				}
+			}
+			return -1;
+		}
+	private:
+		size_type push_unique_impl(T&& val)
+		{
+			size_type index;
+			if (this->find(val, index))
+			{
+				return index;
+			}
+			return push_unique(std::forward<T>(val));
+		}
+		//heapify HeapPush
 	};
 }
